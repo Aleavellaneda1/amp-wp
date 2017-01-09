acbfdc7af0c6843a7934afae00920a3f34f12e81
Del Putnam <del@putnams.net>
2017-01-09 11:11:33 -0500

    More test coverage and comments for all methods.

--- includes/admin/class-amp-cache-utilities.php
+++ includes/admin/class-amp-cache-utilities.php
@@ -5,6 +5,10 @@ abstract class AMP_Cache_Utilities {
 	static $amp_cache_url_base = 'https://cdn.ampproject.org';
 	static $amp_cache_update_url_base = 'https://cdn.ampproject.org/update-ping';
 
+	/**
+	 * Hooks the 'post_updated' and 'before_delete_post' actions to determine if we
+	 * need to update the AMP Cache when a post is updated or about to be deleted.
+	 */
 	public static function amp_add_cache_update_actions() {
 		// Hooking this to the post_updated action so this will fire any time a post
 		// is updated in any way (including status transitions).
@@ -15,50 +19,142 @@ abstract class AMP_Cache_Utilities {
 		add_action( 'before_delete_post',  array( 'AMP_Cache_Utilities', 'do_amp_update_ping' ) );
 	}
 
+	/**
+	 * Called from the 'post_updated' hook when a post is updated. This hook is also fired when
+	 * a post's status changes. If the post is currently published or was not published and now is, 
+	 * then we need to update the cache.
+	 * 
+	 * This function will return true if the cache was updated successfully. It returns
+	 * false if the cache did not need to be updated *or* if the update failed for any
+	 * reason.
+	 * 
+	 * @param  int/WP_Post 	$post_id     The post that was being updated
+	 * @param  WP_Post 		$post_after  A copy of the post after the update.
+	 * @param  WP_Post		$post_before A copy of the post before the update.
+	 * @return bool         true is the cache was updated, false otherwise.
+	 */
+	public static function post_updated( $post_id, $post_after, $post_before ) {
+		// if post_status is 'publish' or was 'publish' but now is not, update cache
+		if ( ( 'publish' == $post_after->post_status ) ||
+			( ( 'publish' != $post_after->post_status ) && ( 'publish' == $post_before->post_status ) ) ) {
+			return self::do_amp_update_ping( $post_id );
+		}
+		return false;
+	}
+
+	/**
+	 * Given a post_id or WP_Post object, this function will calculate the AMP
+	 * cache update URL and ping it.
+	 * 
+	 * This function will return true if the cache was updated successfully. It returns
+	 * false if the cache did not need to be updated *or* if the update failed for any
+	 * reason.
+	 * 
+	 * @param  int/WP_Post	$post 	The post to update in the AMP cache.
+	 * @return bool       			true if the cache was updated, false otherwise.
+	 */
+	public static function do_amp_update_ping( $post ) {
+		$update_ping_url = self::get_amp_cache_update_url_for_post( $post );
+		if ( ! $update_ping_url ) {
+			return false;
+		}
+		$response = wp_remote_get( $update_ping_url );
+		return ( 204 == wp_remote_retrieve_response_code( $response ) );
+	}
+
+	/**
+	 * Given a post_id or WP_Post object, calculste the AMP cache URL based on
+	 * the post's permalink.
+	 *
+	 * See: https://developers.google.com/amp/cache/overview
+	 * 
+	 * @param  int/WP_Post	$post 	The post to get rhe AMP cache url for.
+	 * @return string/bool       	The AMP cache URL on success, false on failure.
+	 */
+	public static function get_amp_cache_url_for_post( $post ) {
+		$amp_cache_resource_path = self::get_amp_cache_path_for_post( $post );
+		if ( $amp_cache_resource_path ) {
+			return self::$amp_cache_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
+		}
+		return false;
+	}
+
+	/**
+	 * Given a post_id or WP_Post object, calculate the AMP cache update URL based
+	 * on the post's permalink.
+	 *
+	 * See: https://developers.google.com/amp/cache/update-ping
+	 *
+	 * @param  int/WP_Post	$post 	The post to get rhe AMP cache update url for.
+	 * @return string/bool       	The AMP cache URL on success, false on failure.
+	 */
+	public static function get_amp_cache_update_url_for_post( $post ) {
+		$amp_cache_resource_path = self::get_amp_cache_path_for_post( $post );
+		if ( $amp_cache_resource_path ) {
+			return self::$amp_cache_update_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
+		}
+		return false;
+	}
+
+	/**
+	 * Given any URL and content type, calculate the AMP cache URL for this resource.
+	 * 
+	 * @param  string 	$url     		The url to calculate the AMP cache URL from.
+	 * @param  string 	$content_type 	Currently only supports 'c' for posts and 'i' for images.
+	 * @param  string 	$scheme       	Optional. 'http' or 'https' If provided, this overrides the
+	 *                                	scheme in the URL. If null, scheme is taken from the URL.
+	 * @return string/bool              The AMP cache URL on success, false on failure.
+	 */
 	public static function get_amp_cache_url_for_resource( $url, $content_type, $scheme = null ) {
-		$amp_cache_resource_path = self::get_amp_cache_path_for_url( $url );
+		$amp_cache_resource_path = self::get_amp_cache_path_for_url( $url, $content_type );
 		if ( $amp_cache_resource_path ) {
 			return self::$amp_cache_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
 		}
 		return false;
 	}
 
+	/**
+	 * Given any URL and content type, calculate the AMP cache update URL for this resource.
+	 * 
+	 * @param  string 	$url     		The url to calculate the AMP cache update URL from.
+	 * @param  string 	$content_type 	Currently only supports 'c' for posts and 'i' for images.
+	 * @param  string 	$scheme       	Optional. 'http' or 'https' If provided, this overrides the
+	 *                                	scheme in the URL. If null, scheme is taken from the URL.
+	 * @return string/bool              The AMP cache udpate URL on success, false on failure.
+	 */
 	public static function get_amp_cache_update_url_for_resource( $url, $content_type, $scheme = null ) {
-		$amp_cache_resource_path = self::get_amp_cache_path_for_url( $url );
+		$amp_cache_resource_path = self::get_amp_cache_path_for_url( $url, $content_type );
 		if ( $amp_cache_resource_path ) {
 			return self::$amp_cache_update_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
 		}
 		return false;
 	}
 
-	public static function get_amp_cache_url_for_post( $post_id ) {
-		$amp_cache_resource_path = self::get_amp_cache_path_for_post( $post_id );
-		if ( $amp_cache_resource_path ) {
-			return self::$amp_cache_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
-		}
-		return false;
-	}
-
-	public static function get_amp_cache_update_url_for_post( $post_id ) {
-		$amp_cache_resource_path = self::get_amp_cache_path_for_post( $post_id );
-		if ( $amp_cache_resource_path ) {
-			return self::$amp_cache_update_url_base . '/' . ltrim( $amp_cache_resource_path, '/' );
-		}
-		return false;
-	}
-
-	public static function get_amp_cache_path_for_post( $post_id, $content_type = null, $scheme = null ) {
-		$permalink = get_permalink( $post_id );
+	/**
+	 * Given a post_id or WP_Post onbject, calculate the AMP cache path part (the part after 
+	 * https://cdn.ampproject.org) for this post.
+	 * 
+	 * @param  int/WP_Post	$post 			The post to get rhe AMP cache path for.
+	 * @param  string 		$content_type 	Currently only supports 'c' for posts and 'i' for images.
+	 *                                 		Optional, if provided, this overrides the actual content
+	 *                                 		type of the post. If null, the content type is determined
+	 *                                 		by post_type.
+	 * @param  string 	$scheme       		Optional. 'http' or 'https' If provided, this overrides the
+	 *                                		scheme in the URL. If null, scheme is taken from the URL.
+	 * @return string/bool              	The AMP cache path part on success, false on failure.
+	 */
+	public static function get_amp_cache_path_for_post( $post, $content_type = null, $scheme = null ) {
+		$permalink = get_permalink( $post );
 
 		// If permalink couldn't be retrieved, return failure.
 		if ( false === $permalink ) {
-			error_log( sprintf( 'ERROR in: %s DETAIL: Couldn\'t get permalink of post with ID %s.', __METHOD__, $post_id ) );
+			error_log( sprintf( 'ERROR in: %s DETAIL: Couldn\'t get permalink of post with ID %s.', __METHOD__, $post ) );
 			return false;
 		}
 
 		// determine $content_type, if not specified
 		if ( null == $content_type ) {
-			$post_type = get_post_type( $post_id );
+			$post_type = get_post_type( $post );
 			switch ( $post_type ) {
 				case 'attachment':
 					$content_type = 'i';
@@ -77,11 +173,21 @@ abstract class AMP_Cache_Utilities {
 		return self::get_amp_cache_path_for_url( $permalink, $content_type, $scheme );
 	}
 
+	/**
+	 * Given a URL and content_type, calculate the AMP cache path part (the part after
+	 * https://cdn.ampproject.org) for this url/content_type
+	 * .
+	 * @param  string 	$url     		The url to calculate the AMP cache path from.
+	 * @param  string 	$content_type 	Currently only supports 'c' for posts and 'i' for images.
+	 * @param  string 	$scheme       	Optional. 'http' or 'https' If provided, this overrides the
+	 *                                	scheme in the URL. If null, scheme is taken from the URL.
+	 * @return string/bool              The AMP cache path part on success, false on failure.
+	 */
 	public static function get_amp_cache_path_for_url( $url, $content_type , $scheme = null ) {
 		$parsed_url = wp_parse_url( $url );
 		// If permalink couldn't be parsed, then return failure.
 		if ( false === $parsed_url ) {
-			error_log( sprintf( 'ERROR in: %s DETAIL: Couldn\'t parse permalink of post with ID %s.', __METHOD__, $post_id ) );
+			error_log( sprintf( 'ERROR in: %s DETAIL: Couldn\'t parse permalink of post with ID %s.', __METHOD__, $post ) );
 			return false;
 		}
 
@@ -147,23 +253,6 @@ abstract class AMP_Cache_Utilities {
 		}
 
 		return $amp_cache_url;
-	}
-
-	public static function post_updated( $post_id, $post_after, $post_before ) {
-		// if post_status is 'publish' or was 'publish' but now is not, update cache
-		if ( ( 'publish' == $post_after->post_status ) ||
-			( ( 'publish' != $post_after->post_status ) && ( 'publish' == $post_before->post_status ) ) ) {
-			return self::do_amp_update_ping( $post_id );
-		}
-	}
-
-	public static function do_amp_update_ping( $post_id ) {
-		$update_ping_url = self::get_amp_cache_update_url_for_post( $post_id );
-		if ( ! $update_ping_url ) {
-			return false;
-		}
-		$response = wp_remote_get( $update_ping_url );
-		return ( 204 == wp_remote_retrieve_response_code( $response ) );
 	}
 }
 ?>
--- tests/test-amp-cache-utilities.php
+++ tests/test-amp-cache-utilities.php
@@ -2,6 +2,8 @@
 
 class AMP_Cache_Utilities_Test extends WP_UnitTestCase {
 
+	private $http_response_code = 0;
+
 	public function get_amp_cache_path_for_url_data() {
 		return array(
 			'http_content_success' => array(
@@ -163,9 +165,359 @@ class AMP_Cache_Utilities_Test extends WP_UnitTestCase {
 	 * @dataProvider get_amp_cache_path_for_url_data
 	 * @group amp-cache-path-test	
 	 */
-	public function test_get_amp_cache_path_for_url( $data, $cexpected_ache_path ) {
+	public function test_get_amp_cache_path_for_url( $data, $expected_ache_path ) {
 		$cache_path = AMP_Cache_Utilities::get_amp_cache_path_for_url( $data['url'], $data['content_type'] , $data['scheme'] );
-		$this->assertEquals( $cache_path, $cexpected_ache_path );
+		$this->assertEquals( $cache_path, $expected_ache_path );
+	}
+
+
+	public function get_do_amp_update_ping_data() {
+		return array(
+			'post_success' => array(
+				array(
+					'factory_type' => 'post',
+					'http_response_code' => 204,
+				),
+				true,
+			),
+			'attachment_success' => array(
+				array(
+					'factory_type' => 'attachment',
+					'http_response_code' => 204,
+				),
+				true,
+			),
+			'tag_fail' => array(
+				array(
+					'factory_type' => 'tag',
+					'http_response_code' => 204,
+				),
+				false,
+			),
+			'user_fail' => array(
+				array(
+					'factory_type' => 'user',
+					'http_response_code' => 204,
+				),
+				false,
+			),
+			'post_fail' => array(
+				array(
+					'factory_type' => 'post',
+					'http_response_code' => 404,
+				),
+				false,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider get_do_amp_update_ping_data
+	 * @group amp-update-ping-test	
+	 */
+	public function test_do_amp_update_ping( $data, $expected ) {
+		add_filter( 'pre_http_request', array( $this, 'mock_update_ping_request' ), 10, 3 );
+		$post_id = $this->factory->{$data['factory_type']}->create();
+		$this->http_response_code = $data['http_response_code'];
+		$result = AMP_Cache_Utilities::do_amp_update_ping( $post_id );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function mock_update_ping_request( $preempt, $request_args, $url ) {
+		$response = array(
+			'response' => array(
+				'code' => $this->http_response_code,
+			),
+		);
+		return $response;
+	}
+
+	public function get_get_amp_cache_update_url_for_resource_data() {
+		return array(
+			'post_success' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/update-ping/c/example.com/path/to/resource',
+			),
+			'post_https_success' => array(
+				array(
+					'url' => 'https://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/update-ping/c/s/example.com/path/to/resource',
+			),
+			'image_success' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'i',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/update-ping/i/example.com/path/to/resource',
+			),
+			'image_https_success' => array(
+				array(
+					'url' => 'https://example.com/path/to/resource',
+					'content_type' => 'i',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/update-ping/i/s/example.com/path/to/resource',
+			),
+			'bad_content_type_fail' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'x',
+					'scheme' => null,
+				),
+				false,
+			),
+			'post_bad_scheme_fail' => array(
+				array(
+					'url' => 'file://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'protocol_relative_scheme_fail' => array(
+				array(
+					'url' => '//example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'no_protocol_scheme_fail' => array(
+				array(
+					'url' => 'example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'host_relative_fail' => array(
+				array(
+					'url' => '/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider get_get_amp_cache_update_url_for_resource_data
+	 * @group amp-cache-url-test	
+	 */
+	public function test_get_amp_cache_update_url_for_resource( $data, $expected ) {
+		add_filter( 'pre_http_request', array( 'AMP_Cache_Utilities_Test', 'handle_update_ping_request' ), 10, 3 );
+		$result = AMP_Cache_Utilities::get_amp_cache_update_url_for_resource( $data['url'], $data['content_type'], $data['scheme'] );
+		$this->assertEquals( $expected, $result );
+	}
+
+
+	public function get_get_amp_cache_url_for_resource_data() {
+		return array(
+			'post_success' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/c/example.com/path/to/resource',
+			),
+			'post_https_success' => array(
+				array(
+					'url' => 'https://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/c/s/example.com/path/to/resource',
+			),
+			'image_success' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'i',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/i/example.com/path/to/resource',
+			),
+			'image_https_success' => array(
+				array(
+					'url' => 'https://example.com/path/to/resource',
+					'content_type' => 'i',
+					'scheme' => null,
+				),
+				'https://cdn.ampproject.org/i/s/example.com/path/to/resource',
+			),
+			'bad_content_type_fail' => array(
+				array(
+					'url' => 'http://example.com/path/to/resource',
+					'content_type' => 'x',
+					'scheme' => null,
+				),
+				false,
+			),
+			'post_bad_scheme_fail' => array(
+				array(
+					'url' => 'file://example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'protocol_relative_scheme_fail' => array(
+				array(
+					'url' => '//example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'no_protocol_scheme_fail' => array(
+				array(
+					'url' => 'example.com/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+			'host_relative_fail' => array(
+				array(
+					'url' => '/path/to/resource',
+					'content_type' => 'c',
+					'scheme' => null,
+				),
+				false,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider get_get_amp_cache_url_for_resource_data
+	 * @group amp-cache-url-test	
+	 */
+	public function test_get_amp_cache_url_for_resource( $data, $expected ) {
+		add_filter( 'pre_http_request', array( 'AMP_Cache_Utilities_Test', 'handle_update_ping_request' ), 10, 3 );
+		$result = AMP_Cache_Utilities::get_amp_cache_url_for_resource( $data['url'], $data['content_type'], $data['scheme'] );
+		$this->assertEquals( $expected, $result );
+	}
+
+
+	public function get_post_updated_data() {
+		return array(
+			'post_updated' => array(
+				array(
+					'factory_type' => 'post',
+					'post_status_before' => 'publish',
+					'post_status_after' => 'publish',
+				),
+				true,
+			),
+			'post_unpublished' => array(
+				array(
+					'factory_type' => 'post',
+					'post_status_before' => 'publish',
+					'post_status_after' => 'draft',
+				),
+				true,
+			),
+			'post_published' => array(
+				array(
+					'factory_type' => 'post',
+					'post_status_before' => 'draft',
+					'post_status_after' => 'publish',
+				),
+				true,
+			),
+			'post_updated_not_published' => array(
+				array(
+					'factory_type' => 'post',
+					'post_status_before' => 'draft',
+					'post_status_after' => 'draft',
+				),
+				false,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider get_post_updated_data
+	 * @group post-updated-test	
+	 */
+	public function test_post_updated( $data, $expected ) {
+		AMP_Cache_Utilities::amp_add_cache_update_actions();
+		$post = $this->factory->{$data['factory_type']}->create_and_get();
+		$post->post_status = $data['post_status_before'];
+		wp_update_post( $post );
+		$post_before = clone $post;
+		$post->post_status = $data['post_status_after'];
+		wp_update_post( $post );
+		$post_after = clone $post;
+		$got = AMP_Cache_Utilities::post_updated( $post->ID, $post_after, $post_before );
+
+		$this->assertEquals( $expected, $got );
+	}
+
+
+	public function get_get_amp_cache_url_for_post_data() {
+		return array(
+			'post_success' => array(
+				array(
+					'factory_type' => 'post',
+					'content_type' => 'c',
+				),
+				true,
+			),
+			'attachment_success' => array(
+				array(
+					'factory_type' => 'attachment',
+					'content_type' => 'i',
+				),
+				true,
+			),
+			'bad_post_type_fail' => array(
+				array(
+					'factory_type' => 'user',
+					'content_type' => 'c',
+				),
+				false,
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider get_get_amp_cache_url_for_post_data
+	 * @group get-amp-cache-url-for-post	
+	 */
+	public function test_get_amp_cache_url_for_post( $data, $expected ) {
+		$post_id = $this->factory->{$data['factory_type']}->create();
+		$got = AMP_Cache_Utilities::get_amp_cache_url_for_post( $post_id );
+
+		if ( $expected ) {
+
+			// replace the scheme with the appropriate cdn url
+			$permalink = get_permalink( $post_id );
+
+			if ( 'c' == $data['content_type'] ) {
+				$expected = preg_replace('@(http[s]{0,1}\:\/\/)@ui', 'https://cdn.ampproject.org/c/', $permalink);
+			} elseif ( 'i' == $data['content_type'] ) {
+				$expected = preg_replace('@(http[s]{0,1}\:\/\/)@ui', 'https://cdn.ampproject.org/i/', $permalink);
+			} else {
+				$expected = false;
+			}
+
+			// if there was not a valid scheme, then we should get false
+			if ( $got == $permalink ) {
+				$expected = false;
+			}
+		}
+
+		$this->assertEquals( $expected, $got );
 	}
 }
 ?>
